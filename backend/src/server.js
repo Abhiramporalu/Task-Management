b0484e825ead869c7aa775544ec4ff3c95d9b891
Abhiram <abhi@gmail.com>
2026-05-02 12:44:02 +0530

    backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -38,9 +38,9 @@ if (process.env.NODE_ENV === 'production') {
   const __dirname = path.resolve();
   app.use(express.static(path.join(__dirname, 'frontend/dist')));
 
-  app.get('(.*)', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'frontend/dist', 'index.html'))
-  );
+  app.get('.*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'frontend/dist', 'index.html'));
+  });
 } else {
   app.get('/', (req, res) => {
     res.send('API is running...');
